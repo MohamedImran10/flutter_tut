637d35bb940c9e30dcedad91a7f14db56e67015a
MohamedImran10 <mohamedimran1040@gmail.com>
2025-09-30 12:33:11 +0530

    api updated

--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import backend
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(backend.urls)),
+    path('',include('backend.urls')),
 ]
